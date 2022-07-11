b2b1155ba01281624784404542ada393771253ab
maxlestage <maxlestage@icloud.com>
2022-07-11 11:08:59 +0200

    IsEven : ✅

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -21,12 +21,17 @@ public class ExerciseRunner {
         // System.out.println(IntOperation.divideTwoIntegers(13, 4));
 
         // FloatOperation
-        System.out.println("Add");
-        System.out.println(FloatOperation.addTwoFloats(1.0f, 2.5f));
-        System.out.println(FloatOperation.addTwoFloats(15.18f, 68.28347f));
+        // System.out.println("Add");
+        // System.out.println(FloatOperation.addTwoFloats(1.0f, 2.5f));
+        // System.out.println(FloatOperation.addTwoFloats(15.18f, 68.28347f));
 
-        System.out.println("Divide");
-        System.out.println(FloatOperation.divideTwoFloats(7.0f, 2.0f));
-        System.out.println(FloatOperation.divideTwoFloats(5.6f, 6.9f));
+        // System.out.println("Divide");
+        // System.out.println(FloatOperation.divideTwoFloats(7.0f, 2.0f));
+        // System.out.println(FloatOperation.divideTwoFloats(5.6f, 6.9f));
+
+        // IsEven
+        System.out.println(IsEven.isEven(2));
+        System.out.println(IsEven.isEven(26));
+        System.out.println(IsEven.isEven(57));
     }
 }
