7d7e0a9d1955110968cae574571ab416d7dbf28e
maxlestage <maxlestage@icloud.com>
2022-07-11 11:05:38 +0200

    FloatOperation : ✅

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -4,20 +4,29 @@ public class ExerciseRunner {
         // System.out.println(HelloWorld.helloWorld());
 
         // IntOperation
+        // System.out.println("Add");
+        // System.out.println(IntOperation.addTwoIntegers(1, 2));
+        // System.out.println(IntOperation.addTwoIntegers(15, 68));
+
+        // System.out.println("Subtract");
+        // System.out.println(IntOperation.subtractTwoIntegers(4, 1));
+        // System.out.println(IntOperation.subtractTwoIntegers(10, 23));
+
+        // System.out.println("Multiply");
+        // System.out.println(IntOperation.multiplyTwoIntegers(3, 6));
+        // System.out.println(IntOperation.multiplyTwoIntegers(12, 11));
+
+        // System.out.println("Divide");
+        // System.out.println(IntOperation.divideTwoIntegers(8, 2));
+        // System.out.println(IntOperation.divideTwoIntegers(13, 4));
+
+        // FloatOperation
         System.out.println("Add");
-        System.out.println(IntOperation.addTwoIntegers(1, 2));
-        System.out.println(IntOperation.addTwoIntegers(15, 68));
-
-        System.out.println("Subtract");
-        System.out.println(IntOperation.subtractTwoIntegers(4, 1));
-        System.out.println(IntOperation.subtractTwoIntegers(10, 23));
-
-        System.out.println("Multiply");
-        System.out.println(IntOperation.multiplyTwoIntegers(3, 6));
-        System.out.println(IntOperation.multiplyTwoIntegers(12, 11));
+        System.out.println(FloatOperation.addTwoFloats(1.0f, 2.5f));
+        System.out.println(FloatOperation.addTwoFloats(15.18f, 68.28347f));
 
         System.out.println("Divide");
-        System.out.println(IntOperation.divideTwoIntegers(8, 2));
-        System.out.println(IntOperation.divideTwoIntegers(13, 4));
+        System.out.println(FloatOperation.divideTwoFloats(7.0f, 2.0f));
+        System.out.println(FloatOperation.divideTwoFloats(5.6f, 6.9f));
     }
 }
