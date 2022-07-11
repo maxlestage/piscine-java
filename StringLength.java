21ff7f11e4cba0e05e881e89cf9ece0e1f9f2656
maxlestage <maxlestage@icloud.com>
2022-07-11 11:38:57 +0200

    StringLength : ✅

--- StringLength.java
+++ StringLength.java
@@ -1,6 +1,6 @@
 public class StringLength {
     public static int getStringLength(String s) {
-        if (s.length() != 0) {
+        if (s != null) {
             return s.length();
         } else {
             return 0;
