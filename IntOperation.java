77d87d305d81cea4e4750d405b7e8bd7e50f1605
maxlestage <maxlestage@icloud.com>
2022-07-11 10:58:16 +0200

    IntOperation :  ✅

---
+++ IntOperation.java
@@ -0,0 +1,17 @@
+public class IntOperation {
+    public static int addTwoIntegers(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtractTwoIntegers(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplyTwoIntegers(int a, int b) {
+        return a * b;
+    }
+
+    public static int divideTwoIntegers(int a, int b) {
+        return a / b;
+    }
+}
