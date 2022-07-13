e573e9171ab5830ef742f196bc148951ad244baf
maxlestage <maxlestage@icloud.com>
2022-07-13 12:44:26 +0200

    ListEqual : 🏗

--- ListEqual.java
+++ ListEqual.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class ListEqual {
     public static boolean areListEquals(List<String> list1, List<String> list2) {
 
-        if (list1 != null || list2 != null) {
+        if (list1 != null || list2 != null || list1 != null && list2 != null) {
             if (list1.get(0) != "" || list2.get(0) != "") {
                 if (list1.size() == list2.size()) {
                     if (list1.equals(list2)) {
