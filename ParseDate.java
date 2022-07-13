c13641a54d0f521d4a5f887db5cea1cbe2521479
maxlestage <maxlestage@icloud.com>
2022-07-13 17:44:46 +0200

    ListEqual : 🏗

--- ParseDate.java
+++ ParseDate.java
@@ -9,38 +9,47 @@ import java.util.Locale;
 public class ParseDate {
 
     public static LocalDateTime parseIsoFormat(String stringDate) {
-        if (stringDate != "") {
-            LocalDateTime dateTime = LocalDateTime.parse(stringDate);
+        if (!stringDate.isBlank()) {
+            if (stringDate != "") {
+                LocalDateTime dateTime = LocalDateTime.parse(stringDate);
 
-            return dateTime;
+                return dateTime;
+            } else {
+                return null;
+            }
         } else {
             return null;
         }
-
     }
 
     public static LocalDate parseFullTextFormat(String stringDate) {
+        if (!stringDate.isBlank()) {
+            if (stringDate != "") {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE dd MMMM yyyy");
+                String text = stringDate.formatted(formatter);
+                LocalDate parsedDate = LocalDate.parse(text, formatter);
 
-        if (stringDate != "") {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE dd MMMM yyyy");
-            String text = stringDate.formatted(formatter);
-            LocalDate parsedDate = LocalDate.parse(text, formatter);
-
-            return parsedDate;
+                return parsedDate;
+            } else {
+                return null;
+            }
         } else {
             return null;
         }
-
     }
 
     public static LocalTime parseTimeFormat(String stringDate) {
-        if (stringDate != "") {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
-                    "hh 'heures' B, mm 'minutes et' ss 'secondes'", Locale.FRANCE);
-            String text = stringDate.formatted(formatter);
-            LocalTime parsedDate = LocalTime.parse(text, formatter);
+        if (!stringDate.isBlank()) {
+            if (stringDate != "") {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
+                        "hh 'heures' B, mm 'minutes et' ss 'secondes'", Locale.FRANCE);
+                String text = stringDate.formatted(formatter);
+                LocalTime parsedDate = LocalTime.parse(text, formatter);
 
-            return parsedDate;
+                return parsedDate;
+            } else {
+                return null;
+            }
         } else {
             return null;
         }
