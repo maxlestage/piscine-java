0abbd4d9b323790ab17d8b9794adac6f01ec034b
maxlestage <maxlestage@icloud.com>
2022-07-14 15:14:44 +0200

    Sort : 🛠

--- Sort.java
+++ Sort.java
@@ -5,14 +5,23 @@ import java.util.stream.Collectors;
 public class Sort {
 
     public static List<Integer> sort(List<Integer> list) {
-        List<Integer> sortedList = list.stream().sorted().collect(Collectors.toList());
-        return sortedList;
+        if (list.size() != 0) {
+            List<Integer> sortedList = list.stream().sorted().collect(Collectors.toList());
+            return sortedList;
+        } else {
+            return null;
+        }
         // [-1, 1, 12, 14, 14, 15, 18, 54, 98]
     }
 
     public static List<Integer> sortReverse(List<Integer> list) {
-        List<Integer> sortedList = list.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
-        return sortedList;
+
+        if (list.size() != 0) {
+            List<Integer> sortedList = list.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+            return sortedList;
+        } else {
+            return null;
+        }
         // [98,54,18,15,14,14,12,1,-1]
     }
 }
